a74e238f02c3515cd8821ddde78f4b0ff80e0b8c
niwanowa <40478818+niwanowa@users.noreply.github.com>
2024-09-04 21:41:10 +0900

    6-2 : done

--- chapter-06/6-2/src/App.js
+++ chapter-06/6-2/src/App.js
@@ -4,5 +4,14 @@ import ColorList from "./ColorList";
 
 export default function App() {
     const [colors, setColors] = useState(colorData);
-    return (<ColorList colors={colors} onRemoveColor={id => { const newColors = colors.filter(color => color.id !== id); setColors(newColors) }} />);
+    return (<ColorList
+        colors={colors}
+        onRemoveColor={id => {
+            const newColors = colors.filter(color => color.id !== id);
+            setColors(newColors)
+        }}
+        onRateColor={(id, rating) => {
+            const newColors = colors.map(color => color.id === id ? { ...color, rating } : color);
+            setColors(newColors)
+        }} />);
 }
--- chapter-06/6-2/src/Color.js
+++ chapter-06/6-2/src/Color.js
@@ -1,7 +1,7 @@
 import StarRating from "./StarRating";
 import { FaTrash } from "react-icons/fa";
 
-export default function Color({ id, title, color, rating, onRemove = f => f }) {
+export default function Color({ id, title, color, rating, onRemove = f => f, onRate = f => f }) {
     return (
         <section>
             <h1>{title}</h1>
@@ -10,7 +10,7 @@ export default function Color({ id, title, color, rating, onRemove = f => f }) {
             </button>
             <div style={{ height: 50, backgroundColor: color }}>
             </div>
-            <StarRating selectedStars={rating} />
+            <StarRating selectedStars={rating} onRate={rating => onRate(id, rating)} />
         </section >
     );
 }
