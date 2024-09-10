43a3987df1acfc4f379d3f7a33230b5b596834a8
niwanowa <40478818+niwanowa@users.noreply.github.com>
2024-09-10 20:26:22 +0900

    6-3-1 : done

--- chapter-06/6-3/src/App.js
+++ chapter-06/6-3/src/App.js
@@ -1,17 +1,35 @@
 import React, { useState } from "react";
 import colorData from "./color-data.json";
 import ColorList from "./ColorList";
+import AddColorForm from "./AddColorForm";
 
 export default function App() {
     const [colors, setColors] = useState(colorData);
-    return (<ColorList
-        colors={colors}
-        onRemoveColor={id => {
-            const newColors = colors.filter(color => color.id !== id);
-            setColors(newColors)
-        }}
-        onRateColor={(id, rating) => {
-            const newColors = colors.map(color => color.id === id ? { ...color, rating } : color);
-            setColors(newColors)
-        }} />);
+    return (
+        <>
+            <AddColorForm onNewColor={(title, color) => {
+                const newColors = [
+                    ...colors,
+                    {
+                        id: Math.random().toString(16).slice(2),
+                        rating: 0,
+                        title,
+                        color
+                    }
+                ];
+                setColors(newColors);
+            }}
+            />
+            <ColorList
+                colors={colors}
+                onRemoveColor={id => {
+                    const newColors = colors.filter(color => color.id !== id);
+                    setColors(newColors)
+                }}
+                onRateColor={(id, rating) => {
+                    const newColors = colors.map(color => color.id === id ? { ...color, rating } : color);
+                    setColors(newColors)
+                }} />
+        </>
+    );
 }
