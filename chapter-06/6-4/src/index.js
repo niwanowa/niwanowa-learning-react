b669476e9af594b06e486bb051cd7b91fc523042
niwanowa <40478818+niwanowa@users.noreply.github.com>
2024-09-11 20:17:07 +0900

    6-4-1 : done

--- chapter-06/6-4/src/index.js
+++ chapter-06/6-4/src/index.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { createContext } from "react";
+import colors from "./color-data.json";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
+export const ColorContext = createContext();
+
 const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+    <ColorContext.Provider value={{ colors }}>
+        <App />
+    </ColorContext.Provider>
+);
